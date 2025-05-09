1d5daa9530918557c0e29d6493262c825f9c9f4e
pablo <pablo.fesqueiras@escola.pr.gov.br>
2025-05-09 11:19:13 -0300

    exemplo5operaçoesstring

---
+++ capitulo 2/exemplo5operaçoesstrings.py
@@ -0,0 +1,11 @@
+# È proibido utilizar o´peraçoes matematicas com strinngs
+# Ao menos em 2 casos
+
+first = 'hellen'
+second = ' gomes'
+nomeSobrenome = first + second # + aqui significa concatenaçao
+print(nomeSobrenome)
+
+stringQualquer = "Teste " * 3
+print(stringQualquer)
+
